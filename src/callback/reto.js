225a15783ee732229c25f71cffc628a2dfc53728
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-06-16 22:06:24 -0500

    Peticiones con la funcion FetchData

--- src/callback/reto.js
+++ src/callback/reto.js
@@ -1,44 +1,58 @@
-//Instancia de request, permite hacer peticiones a la nube, API o URL.
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// importamos el modulo para hacer las peticiones
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+// direccion de la API
+let api = 'https://rickandmortyapi.com/api/character/';
 
-//Nos permite traer la informacion desde nuestra API, desencademos los llamados que necesitamos.
+// funcion principal
 function fetchData(url_api, callback){
-    //Construccion de peticion, referencia a un objeto XMLHTTP
-    let xhhtp = new XMLHttpRequest(); //Aporte de Microsoft
-    //Como hace un llamado a una URL...?
-    xhttp.open('GET', url_api, true);
-    /*
-    Parametro 1: que es lo que queremos hacer, metodo.
-    Parametro 2: URL
-    Parametro 3: Definir si es asincrono o no, por defecto poner 'true'
-     */
-        //Generar un escuchador de la conexion de peticion
-        xhttp.onreadystatechange = function (event){
-            /*validacion de ejecucion, la cual puede tener varios estados 
-            Estado 0: inicializando
-            Estado 1: Cargando
-            Estado 2: Cargado
-            Estado 3: Ya hay algo cargado
-            Estado 4: Completado                */
-            if(xhttp.readystate === 4){
-                if(xhttp.status === 200) {
-                    //Verificar estado, aqui un resumen de los casos mas comunes:
-            /*
-            ESTADO 1xx (100 - 199): Indica que la petición esta siendo procesada.
-            ESTADO 2xx (200 - 299): Indica que la petición fue recibida, aceptada y procesada correctamente.
-            ESTADO 3xx (300 - 399): Indica que hay que tomar acciones adicionales para completar la solicitud. Por lo general indican redireccionamiento.
-            ESTADO 4xx (400 - 499): Errores del lado del cliente. Indica se hizo mal la solicitud de datos.
-            ESTADO 5xx (500 - 599): Errores del Servidor. Indica que fallo totalmente la ejecución.
-            */
-
-            //Estandar de Node con los CallBacks, 1 parametro: error, 2 parametro: resultado.
-                    callback(null, JSON.parse(xhttp.responseText));
-                } else {
-                    const error = new Error('Error' + url_api);
-                    return callback(error, null);
-                }
-            }
-        }
-        //Enviar la solicitud
-        xhttp.send();
+  // instanciamos la conexion
+  let xhttp = new XMLHttpRequest();
+  // abrir una conexion con el metodo, la ruta y si es asincrono
+  xhttp.open('GET', url_api, true);
+  // validacion del llamado
+  xhttp.onreadystatechange = (event) => {
+    // el state 4 es el ultimo de la peticion
+    if(xhttp.readyState === 4){
+      // verificamos que el status este en 200, que dice que todo bien, no un 400 o 500
+      if(xhttp.status === 200){
+        // el primer valor es el err, y el siguiente el resultado
+        // ejecutamos el callback con el resultado
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        // si no es 200
+        let error = new Error('Error: ' + url_api);
+        // matamos el proceso con un error
+        return callback(error, null);
+      }
+    }
+  }
+  // por ultimo enviamos la peticion
+  xhttp.send();
 }
+
+// primero buscamos la lista de personajes
+fetchData(api, (error1, data1) => {
+  // si error, matamos retornando un error
+  if(error1) return console.error(error1);
+  // luego buscamos en la api el id de Rick
+  fetchData(api + data1.results[0].id, (error2, data2) => {
+    // si error, matamos retornando un error
+    if(error2) return console.error(error2);
+    // por ultimo la consulta a la api que contiene su dimension
+    fetchData(data2.origin.url, (error3, data3) => {
+      // si error, matamos retornando un error
+      if(error3) return console.error(error3);
+      
+      // mostramos los resultadoss 
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+      
+      // rutas de las peticiones en orden
+      console.log(api);
+      console.log(api + data1.results[0].id); 
+      console.log(data2.origin.url); 
+    
+    });
+  });
+});
