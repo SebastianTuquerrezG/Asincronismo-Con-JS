01f93740154c3a5206b1ea58dc870fb13d89c748
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-06-17 00:02:05 -0500

    Estructura promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingWillHappen = () => {
     return new Promise((resolve, reject) => {
-        if(false) {
+        if(true) {
             resolve("hey");
         } else {
             reject("whoooops");
@@ -11,3 +11,28 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(response => console.log(response))
     .catch(error => console.log(error));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else {
+            const error = new Error("whoopp!");
+            reject(error);
+        }
+    });
+};
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch(error => {
+        console.error(error);
+    })
